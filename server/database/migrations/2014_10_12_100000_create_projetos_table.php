9d994c560878c9194c59dab16c127ab7d7524201
Pedro Gabriel <pedrpg5@gmail.com>
2023-04-24 11:14:32 -0300

    Iniciando o projeto

---
+++ server/database/migrations/2014_10_12_100000_create_projetos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projetos', function (Blueprint $table) {
+            $table->string('name')->primary();
+            $table->string('horas_gastas');
+            $table->float('valor_hora', 10, 2)->nullable(false);
+            $table->string('status')->nullable(false)->default('aberto');
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('password_reset_tokens');
+    }
+};
